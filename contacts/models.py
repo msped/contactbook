28590699bad3ed443b58d73b8b0f45fef4ed2b59
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-09 20:02:00 +0000

    feat: created Contacts models

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+
+class Contacts(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    phoneNumber = PhoneNumberField(blank=False, null=False, unique=True, max_length=11)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
