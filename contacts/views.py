9f3c6c72161e5df02948cd4186597df4cb8e5c9b
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-09 20:42:37 +0000

    feat: added ContactViews to list all contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import Contacts
+from .serializers import ContactSerializer
 
 # Create your views here.
+
+class ContactsView(ListAPIView):
+    serializer_class = ContactSerializer
+    queryset = Contacts.objects.all()
