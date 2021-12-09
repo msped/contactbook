40edc3cb42fcdc793c28ad4facf3e082a35deeea
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-09 20:56:53 +0000

    feat: added get on ContactView to retrieve specific contact

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,7 @@
 from rest_framework.generics import ListAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 from .models import Contacts
 from .serializers import ContactSerializer
 
@@ -7,3 +10,9 @@ from .serializers import ContactSerializer
 class ContactsView(ListAPIView):
     serializer_class = ContactSerializer
     queryset = Contacts.objects.all()
+
+class ContactView(APIView):
+    def get(self, request, contact_id):
+        contact = Contacts.objects.get(id=contact_id)
+        serializer = ContactSerializer(contact)
+        return Response(serializer.data, status=status.HTTP_200_OK)
