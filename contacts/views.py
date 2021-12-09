b1c865930d65780c0b71c769fb66e13b740528c9
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-09 21:43:11 +0000

    feat: added ContactView put method for updating a contact

--- contacts/views.py
+++ contacts/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from .models import Contacts
 from .serializers import ContactSerializer
 
@@ -13,16 +14,24 @@ class ContactsView(ListAPIView):
 
 class ContactView(APIView):
     def get(self, request, contact_id):
-        contact = Contacts.objects.get(id=contact_id)
+        contact = get_object_or_404(Contacts, id=contact_id)
         serializer = ContactSerializer(contact)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    def put(self, request, contact_id):
+        contact = get_object_or_404(Contacts, id=contact_id)
+        serializer = ContactSerializer(contact, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
     def delete(self, request, contact_id):
-        contact = Contacts.objects.get(id=contact_id)
+        contact = get_object_or_404(Contacts, id=contact_id)
         if contact:
             contact.delete()
             return Response(
                 {f'Contact {contact.name} has been deleted'},
                 status=status.HTTP_200_OK
             )
-        return Response(contact.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"This contact does not exist."}, status=status.HTTP_404_NOT_FOUND)
