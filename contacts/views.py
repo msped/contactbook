878ae104851693d97860ced720de1aafd31ae82d
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-10 00:12:35 +0000

    fix: added serializer.errors to 400 bad request return on contact creation

--- contacts/views.py
+++ contacts/views.py
@@ -17,7 +17,7 @@ class ContactsView(ListCreateAPIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class ContactView(APIView):
     def get(self, request, contact_id):
