20248187a890c98debaa60f13bab42312ce55d36
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-12 17:37:35 +0000

    build: defined media in settings.py

--- contactbook/settings.py
+++ contactbook/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,3 +133,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ]
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
