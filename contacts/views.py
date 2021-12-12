86d46303b0703761a921d26c9b25c7891d51ae5a
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-12 22:55:47 +0000

    refactor: change contactview to use RetreveUpdateDestroyAPIView

--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,7 @@
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework import status
-from django.shortcuts import get_object_or_404
 from .models import Contacts
 from .serializers import ContactSerializer
 
@@ -19,24 +18,10 @@ class ContactsView(ListCreateAPIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class ContactView(APIView):
-    def get(self, request, contact_id):
-        contact = get_object_or_404(Contacts, id=contact_id)
-        serializer = ContactSerializer(contact)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class ContactView(RetrieveUpdateDestroyAPIView):
+    serializer_class = ContactSerializer
+    lookup_url_kwarg = "contact_id"
+    lookup_field = "id"
 
-    def put(self, request, contact_id):
-        contact = get_object_or_404(Contacts, id=contact_id)
-        serializer = ContactSerializer(contact, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, contact_id):
-        contact = get_object_or_404(Contacts, id=contact_id)
-        contact.delete()
-        return Response(
-            {f'Contact {contact.name} has been deleted'},
-            status=status.HTTP_200_OK
-        )
+    def get_queryset(self):
+        return Contacts.objects.all()
