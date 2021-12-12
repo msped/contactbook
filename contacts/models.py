7fa9d2084631d959ad0b289d2d517db6e8788d9d
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-12 17:37:58 +0000

    feat: add profile_picture to contact model

--- contacts/models.py
+++ contacts/models.py
@@ -4,6 +4,10 @@ from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
 
 class Contacts(models.Model):
+    profile_picture = models.ImageField(
+        upload_to="profile_pictures/",
+        default="profile_pictures/default.jpg"
+    )
     name = models.CharField(max_length=50)
     email = models.EmailField()
     phoneNumber = PhoneNumberField(blank=False, null=False, unique=True)
