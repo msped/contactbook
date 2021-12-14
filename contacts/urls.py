0f0d399c32105a18e940315bb939c6f5ae6b3ee7
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-14 23:08:51 +0000

    feat: added update phone number path

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import ContactsView, ContactView
+from .views import ContactsView, ContactView, UpdatePhoneNumber
 
 urlpatterns = [
     path('', ContactsView.as_view(), name="contacts_view"),
-    path('<int:contact_id>', ContactView.as_view(), name="contact_view")
+    path('<int:contact_id>', ContactView.as_view(), name="contact_view"),
+    path(
+        'phone-number/<int:phone_number_id>',
+        UpdatePhoneNumber.as_view(),
+        name="update_phone_number"
+    ),
 ]
