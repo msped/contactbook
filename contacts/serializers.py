737c076e077df00977689f98b9134803b27019dc
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-15 21:46:36 +0000

    refactor: removed update overide on ContactDetail serializer

--- contacts/serializers.py
+++ contacts/serializers.py
@@ -25,7 +25,7 @@ class EmailSerializer(serializers.ModelSerializer):
                 )
             return data
 
-class ContactSerializer(serializers.ModelSerializer):
+class ContactDetailSerializer(serializers.ModelSerializer):
     phone_number = PhoneNumberSerializer(many=True)
     email = EmailSerializer(many=True)
 
@@ -39,32 +39,17 @@ class ContactSerializer(serializers.ModelSerializer):
             'email'
         ]
 
-    def update(self, instance, validated_data):
-        phonenumber_data = validated_data.pop('phone_number')
-        phone_numbers = (instance.phone_number).all()
-        phone_numbers = list(phone_numbers)
-        email_data = validated_data.pop('email')
-        emails = (instance.email).all()
-        emails = list(emails)
-        instance.name = validated_data.get('name', instance.name)
+class ContactsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contacts
+        fields = ['id', 'profile_picture', 'name']
 
-        for number in phonenumber_data:
-            phone_number = phone_numbers.pop(0)
-            phone_number.id = number.get('id', phone_number.id)
-            phone_number.phonenumber_type = number.get(
-                'phonenumber_type',
-                phone_number.phonenumber_type
-            )
-            phone_number.phoneNumber = number.get('phoneNumber', phone_number.phoneNumber)
-            phone_number.save()
+class ProfilePictureSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contacts
+        fields = ['id', 'profile_picture']
 
-        for mail in email_data:
-            email = emails.pop(0)
-            email.id = mail.get('id', email.id)
-            email.email_type = mail.get(
-                'email_type',
-                email.email_type
-            )
-            email.email = mail.get('email', email.email)
-            email.save()
-        return instance
+class ContactNameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contacts
+        fields = ['id', 'name']
