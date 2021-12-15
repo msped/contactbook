8669efbfd4ae4673c35ea13f8210ba9ecc3607fe
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-15 22:23:55 +0000

    refactor: removed unused DestroyAPI view

--- contacts/views.py
+++ contacts/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.generics import (DestroyAPIView, ListCreateAPIView,
+from rest_framework.generics import (ListCreateAPIView,
                                      RetrieveUpdateDestroyAPIView, get_object_or_404)
 from rest_framework.response import Response
 from rest_framework.views import APIView
