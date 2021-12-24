0f2401cc82d101bbe29a2d219835d97a1b987921
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-24 00:17:29 +0000

    feat: added api/auth url to project urls

--- contactbook/urls.py
+++ contactbook/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/contacts/', include('contacts.urls'), name="contactbook_api"),
+    path('api/auth/', include('auth.urls'), name="contactbook_auth"),
 ]
