14cb8e5010a51fd1a53ee6f223b8896eb00f5fd0
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-27 19:24:22 +0000

    feat: added DeleteView to handle user deletion

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import User
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from rest_framework.generics import CreateAPIView, DestroyAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
+from .permissions import IsOwnerOfAccount
 from .serializers import RegisterSerializer
 
 # Create your views here.
@@ -10,3 +11,10 @@ class RegisterView(CreateAPIView):
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+class DeleteView(DestroyAPIView):
+    permission_classes = (IsAuthenticated, IsOwnerOfAccount)
+    queryset = User.objects.all()
+    lookup_field = "id"
+    lookup_url_kwarg = "user_id"
+    
