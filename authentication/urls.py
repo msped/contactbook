c3e341e8c3cee027e6ae5256d6b13a3f5f645c38
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-27 19:24:04 +0000

    feat: added path for user deletion

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
-from .views import RegisterView
+from .views import DeleteView, RegisterView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', RegisterView.as_view(), name="auth_register"),
+    path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register', RegisterView.as_view(), name="auth_register"),
+    path('delete/<int:user_id>', DeleteView.as_view(), name="auth_delete"),
 ]
