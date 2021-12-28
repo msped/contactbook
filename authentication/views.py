09f631785586acc9a19184634896c2b75ad903d7
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-28 22:47:24 +0000

    refactor: updated permission class inline withname change

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView, DestroyAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from .permissions import IsOwnerOfAccount
+from .permissions import IsOwner
 from .serializers import RegisterSerializer
 
 # Create your views here.
@@ -13,7 +13,7 @@ class RegisterView(CreateAPIView):
     serializer_class = RegisterSerializer
 
 class DeleteView(DestroyAPIView):
-    permission_classes = (IsAuthenticated, IsOwnerOfAccount)
+    permission_classes = (IsAuthenticated, IsOwner)
     queryset = User.objects.all()
     lookup_field = "id"
     lookup_url_kwarg = "user_id"
