c99db76d631b6c51fcd06bddb1b61e7822d0ebd2
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-29 23:21:34 +0000

    feat: loaded dotenv to settings for environ variables

--- contactbook/settings.py
+++ contactbook/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&&-!qzgt2txx^*+i$vg_3uyc7^8myuc7m&533651m($rv)$ng&'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
