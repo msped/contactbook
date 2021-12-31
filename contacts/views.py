a9df1cd943649d6b22807d43fc2928eeca896f1e
Matt Edwards <mattsp.edwards@googlemail.com>
2021-12-31 18:48:50 +0000

    feat: added get method to email view

--- contacts/views.py
+++ contacts/views.py
@@ -4,7 +4,7 @@ from rest_framework.generics import (CreateAPIView, ListCreateAPIView,
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.contrib.auth.models import User
+
 from authentication.permissions import IsOwner
 from .models import Contacts, Emails, PhoneNumbers
 from .serializers import (ContactDetailSerializer, ContactNameSerializer,
@@ -83,9 +83,15 @@ class PhoneNumber(APIView):
             status=status.HTTP_204_NO_CONTENT
         )
 
-class UpdateEmail(APIView):
+class Email(APIView):
     serializer_class = EmailSerializer
     permission_classes = (IsAuthenticated, IsOwner)
+
+    def get(self, request, email_id):
+        email = get_object_or_404(Emails, id=email_id)
+        self.check_object_permissions(request, email.contact.owner)
+        serializer = self.serializer_class(email, many=False)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, email_id):
         email = get_object_or_404(Emails, id=email_id)
