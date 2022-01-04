aa11dc571cac54a19a65e73ed87bb905d54b8c91
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-04 22:20:23 +0000

    feat: added BlacklistTokenView

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
+from rest_framework import status
 from rest_framework.generics import CreateAPIView, DestroyAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from .permissions import IsOwner
 from .serializers import RegisterSerializer
@@ -17,4 +21,15 @@ class DeleteView(DestroyAPIView):
     queryset = User.objects.all()
     lookup_field = "id"
     lookup_url_kwarg = "user_id"
-    
+
+class BlacklistTokenView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_200_OK)
+        except Exception as error:
+            return Response({'error': error}, status=status.HTTP_400_BAD_REQUEST)
