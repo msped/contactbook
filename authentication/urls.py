9476685621e0d9cb0329279515a66bff1c816ee5
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-04 22:23:18 +0000

    added: logout/blacklist route to urls.py

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
-from .views import DeleteView, RegisterView
+from .views import DeleteView, RegisterView, BlacklistTokenView
 
 urlpatterns = [
     path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('register', RegisterView.as_view(), name="auth_register"),
     path('delete/<int:user_id>', DeleteView.as_view(), name="auth_delete"),
+    path('logout/blacklist', BlacklistTokenView.as_view(), name="auth_logout"),
 ]
