8688ae28809e1c58c84036285f977bde42e335d0
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-09 18:25:37 +0000

    fix: removed trailing slash in CORS_ALLOW_ORIGINS

--- contactbook/settings.py
+++ contactbook/settings.py
@@ -137,8 +137,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000/',
-    'http://127.0.0.1:3000/',
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
 ]
 
 REST_FRAMEWORK = {
