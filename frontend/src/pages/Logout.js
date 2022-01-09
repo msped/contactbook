e7aaad2fb7987ed5274fab37c0beb865274cfc5e
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-09 21:54:24 +0000

    feat: added logout view

---
+++ frontend/src/pages/Logout.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import api from '../axios';
+import { useNavigate } from 'react-router-dom';
+
+export default function Logout() {
+	const history = useNavigate();
+
+	useEffect(() => {
+		const response = api.post('auth/logout/blacklist', {
+			refresh_token: localStorage.getItem('refresh_token'),
+		});
+		localStorage.removeItem('access_token');
+		localStorage.removeItem('refresh_token');
+		api.defaults.headers['Authorization'] = null;
+		history('/login');
+	});
+	return <div>Logout</div>;
+}
