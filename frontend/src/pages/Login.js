d832938cbdad61256b1bb15c71b71476e9d54029
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-09 18:35:58 +0000

    feat: added login page and api request

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import api from '../axios';
+import { useNavigate } from 'react-router-dom';
+
+
+function Copyright(props) {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="http;//127.0.0.1:8000/">
+        Contact Book
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+export default function Login() {
+    const history = useNavigate()
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        api.post('auth/token', {
+            username: username,
+            password: password
+        })
+        .then((res) => {
+            localStorage.setItem('access_token', res.data.access)
+            localStorage.setItem('refresh_token', res.data.refresh)
+            api.defaults.headers['Authorization'] =
+                'Bearer ' + localStorage.getItem('access_token')
+            history('/')
+        })
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <Box
+            sx={{
+                marginTop: 8,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}
+            >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Login
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="username"
+                    autoComplete="email"
+                    autoFocus
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextField
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                Login
+                </Button>
+                <Grid container>
+                    <Grid item>
+                        <Link href="/register" variant="body2">
+                        {"Don't have an account? Register"}
+                        </Link>
+                    </Grid>
+                </Grid>
+            </Box>
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+        </ThemeProvider>
+    );
+}
