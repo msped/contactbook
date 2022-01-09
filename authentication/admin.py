0a9d828ef46d08a1c95339906b2b2f8e4fb2bbdc
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-09 21:54:02 +0000

    feat: added delete permission for oustanding tokens

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from rest_framework_simplejwt import token_blacklist
+from rest_framework_simplejwt.token_blacklist.admin import OutstandingTokenAdmin
 
 # Register your models here.
+
+class OutstandingTokenAdminNew(OutstandingTokenAdmin):
+
+    def has_delete_permission(self, *args, **kwargs):
+        return True
+
+admin.site.unregister(token_blacklist.models.OutstandingToken)
+admin.site.register(token_blacklist.models.OutstandingToken, OutstandingTokenAdminNew)
