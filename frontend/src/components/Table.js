b9a22067769cda94e5cdd3d308949c0750a0b765
Matt Edwards <mattsp.edwards@googlemail.com>
2022-01-15 17:38:39 +0000

    feat: added reusable table to show email and phonenumber data

---
+++ frontend/src/components/Table.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+import { getPhoneNumberType } from '../utils'
+
+export default function ContentTable({ items, table }) {
+    
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Type</TableCell>
+                        <TableCell align="center">{table}</TableCell>
+                        <TableCell align="center"></TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                {items.map((row) => (
+                    <TableRow
+                        key={row.name}
+                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {getPhoneNumberType(row.phonenumber_type)}
+                            </TableCell>
+                            <TableCell align="center">{row.phoneNumber}</TableCell>
+                            <TableCell align="center">Edit button here</TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
+}
